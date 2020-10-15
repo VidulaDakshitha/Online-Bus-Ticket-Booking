5c335f2776d7b928eaaf2e292ee6bb9258ac0110
VidulaDakshitha <viduladakshitha@gmail.com>
2020-10-15 14:19:57 +0530

    made change

--- Frontend/src/views/Pages/Login/Login.js
+++ Frontend/src/views/Pages/Login/Login.js
@@ -73,7 +73,7 @@ if(username==="admin@gmail.com" && password==="admin")
        }else if(moment(moment(data.val().issueDate).format("YYYY-MM-DD")).isBefore(moment(new Date()).format("YYYY-MM-DD")) && data.val().tokentype==="single")
           {
 
-            database.ref(`token/${data.key}/`).update({isactive:0})
+            database.ref(`token/${data.key}/`).update({isactive:0,amount:0})
           }
 
 
