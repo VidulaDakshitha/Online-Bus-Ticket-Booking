f10bab868d24427e4ac01a6c2957101bb64f9705
VidulaDakshitha <viduladakshitha@gmail.com>
2020-10-15 17:23:58 +0530

    made change

--- Frontend/src/views/Pages/Login/Login.js
+++ Frontend/src/views/Pages/Login/Login.js
@@ -66,11 +66,11 @@ if(username==="admin@gmail.com" && password==="admin")
             localStorage.setItem("tokenType",data.val().tokentype)
 
 
-       if(moment(data.val().expiryDate).isBefore(moment(new Date()).format("YYYY-MM-DD")) && data.val().tokentype==="monthly")
+       if(moment(data.val().expiryDate).isBefore(moment(new Date()).format("YYYY-MM-DD")) && data.val().tokentype!=="single")
        {
         database.ref(`token/${data.key}/`).update({isactive:0,amount:0})
 
-       }else if(moment(moment(data.val().issueDate).format("YYYY-MM-DD")).isBefore(moment(new Date()).format("YYYY-MM-DD")) && (data.val().tokentype==="single"||data.val().tokentype==="temporary"))
+       }else if(moment(moment(data.val().issueDate).format("YYYY-MM-DD")).isBefore(moment(new Date()).format("YYYY-MM-DD")) && data.val().tokentype==="single")
           {
 
             database.ref(`token/${data.key}/`).update({isactive:0,amount:0})
